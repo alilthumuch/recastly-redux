b344486b61ce6c9a941e733c99179fc418006b04
alilthumuch <alilthumuch@gmail.com>
2018-08-29 20:22:49 -0700

    recastly redux

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,7 +2,14 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+const mapDispatchToProps = {
+  handleVideoListEntryTitleClick: changeVideo, 
+};
+const mapStateToProps = (state) => ({
+  videos: state.videoList
+});
+
+var VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
